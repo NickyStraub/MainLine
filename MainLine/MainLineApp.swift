c190b6e9dde8231a1e411d0da95bf008b88f07aa
Nicholas <nickystraub560@gmail.com>
2022-09-09 11:02:14 -0400

    Initial Commit

---
+++ MainLine/MainLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainLineApp.swift
+//  MainLine
+//
+//  Created by Nicholas Straub on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MainLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
